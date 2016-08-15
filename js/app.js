4fbe69d70cb2742884cf33fa5efd1bce7b2404f2
Carlos Rodriguez <carlos@rodriguez.eu.com>
2016-08-15 16:48:09 +0200

    All functionality added; todo: add levels for timer frequency?

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 
-
+/**
+ * Application model
+ */
 var model = {
   MAX_TABLES: 12,
   INITIAL_TIMER: 30,
   CORRECT_INCREMENT: 2,
+  lapseDecrement: 1,
+  totalDuration: 0,
   tables: [],
   operands: [],
   cntOperations: 0,
@@ -11,50 +15,89 @@ var model = {
   maxOperand: 0,
   level: 0,
   progressValue: 0,
+  /**
+   * Model initiaisation
+   */
   init: function() {
     //model.tables = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     //model.maxOperand = 12;
     model.level = 1;
     model.progressValue = model.INITIAL_TIMER;
+    model.lapseDecrement = 1;
+    model.totalDuration = 0;
+    model.cntOperations = 0;
+    model.cntCorrect = 0;
   }
 };
 
+/**
+ * Application octopus (controller)
+ */
 var octopus = {
+  // timer interval
   interval: [],
+  /**
+   * Octopus initialisation
+   */
   init: function() {
     model.init();
     viewModel.init();
   },
+  /**
+   * Initialise practice
+   */
   initPractice: function(tables, operands) {
     // Update model tables and operands with initial selections
     model.tables = tables;
     model.operands = operands;
+    // Get first operation
+    viewModel.nextOperation();
     // Start timer for updating progress bar
     octopus.interval = setInterval(function() {
-      console.log('progress:', model.progressValue, viewModel.barProgress());
+      // Update total duration
+      model.totalDuration++;
+      // Check current progress
       if (model.progressValue > 0 ) {
-        model.progressValue--;
+        // If still positive, update progress
+        model.progressValue -= model.lapseDecrement;
         viewModel.updateBarProgress( Math.round(octopus.getProgressPct()) );
+        // Increase Lapse Decrement, every 30 secs
+        if (model.totalDuration >= 90) {
+          model.lapseDecrement = 3;
+        } else if (model.totalDuration >= 60) {
+          model.lapseDecrement = 2;
+        }
       } else {
         octopus.finishPractice();
       }
+      console.log('Total:', model.totalDuration, 'Decrementing:', model.lapseDecrement);
     }, 1000);
   },
+  /**
+   * Finish practice: stop timer and show results view
+   */
   finishPractice: function() {
     clearInterval(octopus.interval);
     viewModel.finishPractice();
   },
-  setTables: function(tables) {
-    // Check tables is a list with values between 2-12
-  },
+  /**
+   * Get randomly next table, out of selected tables list
+   */
   getTable: function() {
     return model.tables[Math.floor( Math.random() * model.tables.length )];
   },
+  /**
+   * Get randomly next operand, out of selected operands list
+   */
   getOperand: function() {
     return model.operands[Math.floor( Math.random() * model.operands.length )];
     //return Math.floor( Math.random() * model.maxOperand ) + 1;
   },
+  /**
+   * Check input result and update counters and progress bar
+   */
   checkResult: function() {
+    // Check if input result is correct
     if((viewModel.table() * viewModel.operand()) == Number(viewModel.inputResult())) {
       // Correct answer
       model.progressValue += model.CORRECT_INCREMENT;
@@ -66,39 +109,63 @@ var octopus = {
     }
     viewModel.cntOperations(++model.cntOperations);
   },
+  /**
+   * Return progress percentage
+   */
   getProgressPct: function() {
     return model.progressValue * 100.0 / model.INITIAL_TIMER;
   }
 };
 
+/**
+ * Application ViewModel
+ */
 var viewModel = {
-  // List of available Tables and Operands to be selected in initial form
+  // List of available Tables to be selected in initial form
   availableTables: ko.observable([2,3,4,5,6,7,8,9,10,11,12]),
+  // List of available  Operands to be selected in initial form
   availableOperands: ko.observable([2,3,4,5,6,7,8,9,10,11,12]),
-  table: ko.observable(0),
-  //allTables: ko.observable(true),
+  // List of selected Tables and Operands to practice
   selectedTables: ko.observableArray([2,3,4,5,6,7,8,9,10,11,12]),
   selectedOperands: ko.observableArray([2,3,4,5,6,7,8,9,10,11,12]),
+  // table in opeation displayed (table x operand)
+  table: ko.observable(0),
+  // operand in opeation displayed (table x operand)
   operand: ko.observable(0),
-  //allOperands: ko.observable(true),
-  //selOperands: ko.observableArray(),
+  /**
+   * KO computed with operation (table x operand)
+   */
   operation: ko.pureComputed(function(){
     return viewModel.table() + ' x ' + viewModel.operand() + ' = ';
   }, this),
+  // Manual input of result
   inputResult: ko.observable(''),
+  // Number of correct operations
   correctAnswers: ko.observable(0),
+  // Total of operations
   cntOperations: ko.observable(0),
+  /**
+   * Get next operation
+   */
   nextOperation: function() {
     viewModel.table( octopus.getTable() );
     viewModel.operand( octopus.getOperand() );
   },
+  /**
+   * Called when submitting a result
+   */
   submitResult: function() {
     octopus.checkResult();
     viewModel.inputResult('');
     // Check result
     viewModel.nextOperation();
   },
+  // Progress bar value
   barProgress: ko.observable(0),
+  /**
+   * Update progress bar, based on new percentage provided.
+   * It changes colour below 25 and 25 % progress.
+   */
   updateBarProgress: function(newPct) {
     viewModel.barProgress( newPct );
     var el = $('.progress-bar');
@@ -110,8 +177,10 @@ var viewModel = {
       el.addClass('progress-bar-danger').removeClass('progress-bar-success').removeClass('progress-bar-warning');
     }
   },
+  /**
+   * Called after input configuration form, and sets selected Tables and Operands
+   */
   submitInitialConfig: function() {
-    console.log('Initial form submitted:', viewModel.selectedTables(), viewModel.selectedOperands());
     // Hide Practice Settings
     $('.intro-settings').hide();
     // Unhide main view
@@ -119,13 +188,16 @@ var viewModel = {
     // Start Practice
     octopus.initPractice(viewModel.selectedTables(), viewModel.selectedOperands());
   },
+  /**
+   * Called once the practice is finished, to show results view
+   */
   finishPractice: function() {
     $('.main-view').hide();
     $('.results-view').show();
   },
-  // selections: function() {
-  //   console.log('All Tables:', viewModel.allTables(), "All Operands:", viewModel.allOperands());
-  // },
+  /**
+   * Called after input configuration form, and sets selected Tables and Operands
+   */
   init: function() {
     // Initial settings for options selection
     $('#table-selection').multiselect({
@@ -138,9 +210,10 @@ var viewModel = {
     });
     // Initial setting of Progress bar
     viewModel.barProgress( octopus.getProgressPct() );
-    viewModel.nextOperation();
-
   },
+  /**
+   * Called to restart practice
+   */
   startOver: function() {
     octopus.init();
     $('.intro-settings').show();
@@ -153,20 +226,3 @@ var viewModel = {
 
 ko.applyBindings(viewModel);
 octopus.init();
-
-
-// var MAX_COUNT = 10000;
-// var cntTable = [0,0,0,0,0,0,0,0,0,0,0,0,0];
-// var cntOperand = [0,0,0,0,0,0,0,0,0,0,0,0,0];
-// var cnt;
-// for(cnt = 0; cnt < MAX_COUNT; cnt++) {
-//   cntTable[octopus.getTable()]++;
-//   cntOperand[octopus.getOperand()]++;
-// }
-// for(cnt = 0; cnt < cntTable.length; cnt++) {
-//   cntTable[cnt] /= MAX_COUNT;
-// }
-// for(cnt = 0; cnt < cntOperand.length; cnt++) {
-//   cntOperand[cnt] /= MAX_COUNT;
-// }
-// console.log(cntTable, cntOperand);
